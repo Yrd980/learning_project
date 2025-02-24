91900e4199e8a65dd455a8530cb86a1dc4125fac
Yrd980 <yrd980@outlook.com>
2025-02-24 15:44:09 +0800

    fix spell problem add comment

--- minimind/model/model.py
+++ minimind/model/model.py
@@ -1,12 +1,15 @@
 import torch
 import torch.nn.functional as F
 from torch import nn
-from typing import Optional, Tuple
+from typing import Optional, Tuple, List
 import math
+
 from .LMConfig import LMConfig
+from transformers import PreTrainedModel
+from transformers.modeling_outputs import CausalLMOutputWithPast
 
 
-class RNSNorm(torch.nn.Module):
+class RMSNorm(torch.nn.Module):
     def __init__(self, dim: int, eps: float):
         super().__init__()
         self.eps = eps
@@ -20,7 +23,7 @@ class RNSNorm(torch.nn.Module):
         ).type_as(x)
 
 
-def procompute_pos_cis(dim: int, end: int = int(32 * 1024), theta: float = 1e6):
+def precompute_pos_cis(dim: int, end: int = int(32 * 1024), theta: float = 1e6):
     # // 2 because 2i and 2i+1 in sin cos equal so save half
     freqs = 1.0 / (theta ** (torch.arange(0, dim, 2)[: (dim // 2)].float() / dim))
     t = torch.arange(end, device=freqs.device)
@@ -209,7 +212,7 @@ class MoEGate(nn.Module):
                 )
                 # apply n_routed_experts cumulate and topk_idx_for_aux_loss as index aka merge experts occur numbers
                 # topk_idx_for_aux_loss(batch_size * seq_len,top_k)
-                ce.scatter_add(
+                ce.scatter_add_(
                     1,
                     topk_idx_for_aux_loss,
                     torch.ones(bsz, seq_len * aux_topk, device=hidden_states.device),
@@ -240,3 +243,186 @@ class MoEGate(nn.Module):
         else:
             aux_loss = 0
         return topk_idx, topk_weight, aux_loss
+
+
+class MoEFeedForward(nn.Module):
+    def __init__(self, config: LMConfig):
+        super().__init__()
+        self.config = config
+        self.experts = nn.ModuleList(
+            [FeedForword(config) for _ in range(config.n_routed_experts)]
+        )
+        self.gate = MoEGate(config)
+        if config.n_shared_experts is not None:
+            self.shared_experts = FeedForword(config)
+
+    def forward(self, x):
+        identity = x
+        orig_shape = x.shape
+        topk_idx, topk_weight, aux_loss = self.gate(x)
+        x = x.view(-1, x.shape[-1])
+        flat_topk_idx = topk_idx.view(-1)
+        if self.training:
+            x = x.repeat_interleave(self.config.num_experts_per_tok, dim=0)
+            y = torch.empty_like(x, dtype=torch.float16)
+            for i, expert in enumerate(self.experts):
+                y[flat_topk_idx == i] = expert[x[flat_topk_idx == i]].to(y.dtype)
+            y = y.view(*topk_weight.shape, -1) * topk_weight.unsqueeze(-1).sum(dim=1)
+            y = y.view(*orig_shape)
+        else:
+            y = self.moe_infer(x, flat_topk_idx, topk_weight.view(-1, 1)).view(
+                *orig_shape
+            )
+        if self.config.n_shared_experts is not None:
+            y += self.shared_experts(identity)
+        self.aux_loss = aux_loss
+        return y
+
+    @torch.no_grad
+    def moe_infer(self, x, flat_expert_indices, flat_expert_weights):
+        expert_cache = torch.zeros_like(x)
+        idxs = flat_expert_indices.argsort()
+        # calculate end_idx
+        tokens_per_expert = flat_expert_indices.bincount().cpu().numpy().cumsum(0)
+        # expert index
+        token_idxs = idxs // self.config.num_experts_per_tok
+        for i, end_idx in enumerate(tokens_per_expert):
+            start_idx = 0 if i == 0 else tokens_per_expert[i - 1]
+            if start_idx == end_idx:
+                continue
+            expert = self.experts[i]
+            exp_token_idx = token_idxs[start_idx:end_idx]
+            expert_tokens = x[exp_token_idx]
+            expert_out = expert(expert_tokens).to(expert_cache.dtype)
+            expert_out.mul_(flat_expert_weights[idxs[start_idx:end_idx]])
+            expert_cache.scatter_add_(
+                0, exp_token_idx.view(-1, 1).repeat(1, x.shape[-1]), expert_out
+            )
+
+        return expert_cache
+
+
+class MiniMindBlock(nn.Module):
+    def __init__(self, layer_id: int, config: LMConfig):
+        super().__init__()
+        self.n_head = config.n_head
+        self.dim = config.dim
+        self.head_dim = config.dim // config.n_heads
+        self.attention = Attention(config)
+
+        self.layer_id = layer_id
+        self.attention_norm = RMSNorm(config.dim, eps=config.norm_eps)
+        self.ffn_norm = RMSNorm(config.dim, eps=config.eps)
+        self.feed_forward = (
+            FeedForword(config) if not config.use_moe else MoEFeedForward(config)
+        )
+
+    def forward(self, x, pos_cis, past_key_value, use_cache=False):
+        h_attn, past_kv = self.attention(
+            self.attention_norm(x),
+            pos_cis,
+            past_key_value=past_key_value,
+            use_cache=use_cache,
+        )
+        h = x + h_attn
+        out = h + self.feed_forward(self.ffn_norm)
+        return out, past_kv
+
+
+class MiniMindLM(PreTrainedModel):
+    config_class = LMConfig
+
+    def __init__(self, params: LMConfig = None):
+        self.params = params or LMConfig
+        super().__init__(self.params)
+        self.vocab_size, self.n_layers = params.vocab_size, params.n_layers
+        self.tok_embeddings = nn.Embedding(params.vocab_size, params.dim)
+        self.dropout = nn.Dropout(params.dropout)
+        self.layers = nn.ModuleList(
+            [MiniMindBlock(l, params) for l in range(self.n_layers)]
+        )
+        self.norm = RMSNorm(params.dim, eps=params.norm_eps)
+        self.output = nn.Linear(params.dim, params.vocab_size, bias=False)
+        self.tok_embeddings.weight = self.output.weight
+        self.register_buffer(
+            "pos_cis",
+            precompute_pos_cis(
+                dim=params.dim // params.n_heads, theta=params.rope_theta
+            ),
+            persistent=False,
+        )
+        self.OUT = CausalLMOutputWithPast()
+
+    def forward(
+        self,
+        input_ids: Optional[torch.Tensor] = None,
+        past_key_values: Optional[List[Tuple[torch.Tensor, torch.Tensor]]] = None,
+        use_cache: bool = False,
+        **args,
+    ):
+        past_key_values = past_key_values or [None] * len(self.layers)
+        start_pos = args.get('start_pos',0)
+        h = self.dropout(self.tok_embeddings(input_ids))
+        pos_cis = self.pos_cis[start_pos:start_pos+input_ids.size(1)]
+        past_kvs = []
+        for l,layer in enumerate(self.layers):
+            h,past_kv = layer(
+                h,pos_cis,
+                past_key_value=past_key_values[l],
+                use_cache = use_cache
+            )
+            past_kvs.append(past_kv)
+        logits = self.output(self.norm(h))
+        aux_loss = sum(l.feed_forward.aux_loss for l in self.layers if isinstance(l.feed_forward,MoEFeedForward))
+        self.OUT.__setitem__('logits',logits)
+        self.OUT.__setitem__('aux_loss',aux_loss)
+        self.OUT.__setitem__('past_key_values',past_key_values)
+        return self.OUT
+
+    @torch.inference_mode()
+    def generate(self, input_ids, eos_token_id=2, max_new_tokens=1024, temperature=0.75, top_p=0.90,
+                 stream=False, rp=1., use_cache=True, pad_token_id=0, **args):
+        if stream:
+            return self._stream(input_ids,eos_token_id,max_new_tokens,temperature,top_p,rp,use_cache,**args)
+        generated = []
+        for i in range(input_ids.size(0)):
+            non_pad = input_ids[i][input_ids[i]!=pad_token_id].unsqueeze(0)
+            out = self._stream(non_pad,eos_token_id,max_new_tokens,temperature,top_p,rp,use_cache,**args)
+            tokens_list = [tokens[:,-1] for tokens in out]
+            gen = torch.cat(tokens_list,dim=-1) if tokens_list else non_pad
+            full_sequence = torch.cat([non_pad,gen],dim=-1)
+            generated.append(full_sequence)
+        max_length = max(seq.size(1) for seq in generated)
+        generated = [
+            torch.cat(
+                [seq,torch.full((1,max_length-seq.size(1)),pad_token_id,dtype=seq.dtype,device=seq.device)],
+                dim = -1
+            )
+            for seq in generated
+        ]
+        return torch.cat(generated,dim=0)
+    
+    def _stream(self,input_ids,eos_token_id,max_new_tokens,temperature,top_p,rp,use_cache,**args):
+        start,first_seq,past_kvs = input_ids[1],True,None
+        while input_ids.shape[1] < max_new_tokens - 1:
+            if first_seq or not use_cache:
+                out,first_seq = self(input_ids,past_key_values=past_kvs,use_cache = use_cache,**args),False
+            else:
+                out = self(input_ids[:-1],past_key_values=past_kvs,use_cache=use_cache,start_pos=input_ids.shape[1]-1,**args)
+            logits,past_kvs = out.logits[:-1,:],out.past_key_values
+            logits[:,list(set(input_ids.tolist()[0]))] /= rp
+            logits /= (temperature+1e-9)
+            if top_p is not None and top_p < 1.0:
+                sorted_logits , sorted_indices = torch.sort(logits,descending=True,dim=-1)
+                sorted_probs = F.softmax(sorted_logits,dim=-1)
+                cumulative_probs = torch.cumulative(sorted_probs,dim=-1)
+                sorted_indices_to_remove = cumulative_probs > top_p 
+                sorted_indices_to_remove[:,1] = sorted_indices_to_remove[:,:-1].clone()
+                sorted_indices_to_remove[:,0] = False
+                indices_to_remove = sorted_indices_to_remove.scatter_add_(1,sorted_indices,sorted_indices_to_remove)
+                logits[indices_to_remove] = -float('Inf')
+            input_ids_next = torch.multinomial(F.softmax(logits,dim=-1),num_samples=1)
+            input_ids = torch.cat((input_ids,input_ids_next),dim=1)
+            yield input_ids[:start:]
+            if input_ids_next.item() == eos_token_id:
+                break
