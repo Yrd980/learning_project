4b9320e54cbc39e8f3012b563515226097a3dbbd
Yrd980 <yrd980@outlook.com>
2025-06-07 23:07:40 +0800

    add train_lora.py

--- minimind/model/model_minimind.py
+++ minimind/model/model_minimind.py
@@ -58,7 +58,7 @@ class MiniMindConfig(PretrainedConfig):
 
 import torch
 import torch.nn.functional as F
-from torch import nn 
+from torch import nn
 from typing import Optional, Tuple, List, Union
 import math
 from transformers.activations import ACT2FN
@@ -381,29 +381,40 @@ class MiniMindBlock(nn.Module):
 
         self.layer_id = layer_id
         self.input_layernorm = RMSNorm(config.hidden_size, eps=config.rms_norm_eps)
-        self.post_attention_layernorm = RMSNorm(config.hidden_size, eps=config.rms_norm_eps)
-        self.mlp = (
-            FeedForward(config) if not config.use_moe else MoEFeedForward(config)
+        self.post_attention_layernorm = RMSNorm(
+            config.hidden_size, eps=config.rms_norm_eps
         )
+        self.mlp = FeedForward(config) if not config.use_moe else MoEFeedForward(config)
 
-    def forward(self, hidden_states, position_embeddings, past_key_value=None, use_cache=False, attention_mask=None):
+    def forward(
+        self,
+        hidden_states,
+        position_embeddings,
+        past_key_value=None,
+        use_cache=False,
+        attention_mask=None,
+    ):
         residual = hidden_states
         hidden_states, present_key_value = self.self_attn(
             self.input_layernorm(hidden_states),
             position_embeddings,
             past_key_value,
             use_cache,
-            attention_mask
+            attention_mask,
         )
         hidden_states += residual
         hidden_states += self.mlp(self.post_attention_layernorm(hidden_states))
         return hidden_states, present_key_value
 
+
 class MiniMindModel(nn.Module):
     def __init__(self, config: MiniMindConfig):
         super().__init__()
         self.config = config
-        self.vocab_size, self.num_hidden_layers = config.vocab_size, config.num_hidden_layers
+        self.vocab_size, self.num_hidden_layers = (
+            config.vocab_size,
+            config.num_hidden_layers,
+        )
         self.embed_tokens = nn.Embedding(config.vocab_size, config.hidden_size)
         self.dropout = nn.Dropout(config.dropout)
         self.layers = nn.ModuleList(
@@ -414,10 +425,10 @@ class MiniMindModel(nn.Module):
         freqs_cos, freqs_sin = precompute_freqs_cis(
             dim=config.hidden_size // config.num_attention_heads,
             end=config.max_position_embeddings,
-            theta=config.rope_theta
+            theta=config.rope_theta,
         )
 
-        self.register_buffer("freqs_cos", freqs_cos ,persistent=False)
+        self.register_buffer("freqs_cos", freqs_cos, persistent=False)
         self.register_buffer("freqs_sin", freqs_sin, persistent=False)
 
     def forward(
@@ -430,66 +441,79 @@ class MiniMindModel(nn.Module):
     ):
         batch_size, seq_length = input_ids.shape
         past_key_values = past_key_values or [None] * len(self.layers)
-        start_pos = past_key_values[0][0].shape[1] if past_key_values[0] is not None else 0
+        start_pos = (
+            past_key_values[0][0].shape[1] if past_key_values[0] is not None else 0
+        )
 
         hidden_states = self.dropout(self.embed_tokens(input_ids))
 
         position_embeddings = (
-            self.freqs_cos[start_pos: start_pos + seq_length],
-            self.freqs_sin[start_pos: start_pos + seq_length]
+            self.freqs_cos[start_pos : start_pos + seq_length],
+            self.freqs_sin[start_pos : start_pos + seq_length],
         )
 
         presents = []
-        for layer_idx, (layer,past_key_value) in enumerate(zip(self.layers,past_key_values)):
+        for layer_idx, (layer, past_key_value) in enumerate(
+            zip(self.layers, past_key_values)
+        ):
             hidden_states, present = layer(
                 hidden_states,
                 position_embeddings,
                 past_key_value=past_key_value,
                 use_cache=use_cache,
-                attention_mask = attention_mask
+                attention_mask=attention_mask,
             )
             presents.append(present)
 
         hidden_states = self.norm(hidden_states)
 
         aux_loss = sum(
-            layer.mlp.aux_loss for layer in self.layers if isinstance(layer.mlp,MoEFeedForward)
+            layer.mlp.aux_loss
+            for layer in self.layers
+            if isinstance(layer.mlp, MoEFeedForward)
         )
 
         return hidden_states, presents, aux_loss
 
+
 class MiniMindForCausalLM(MiniMindModel):
     config_class = MiniMindConfig
+
     def __init__(self, config: MiniMindConfig = None):
         self.config = config or MiniMindConfig()
         super().__init__(self.config)
         self.model = MiniMindModel(self.config)
-        self.lm_head = nn.Linear(self.config.hidden_size, self.config.vocab_size,bias=False)
+        self.lm_head = nn.Linear(
+            self.config.hidden_size, self.config.vocab_size, bias=False
+        )
         self.model.embed_tokens.weight = self.lm_head.weight
         self.OUT = CausalLMOutputWithPast()
+
 
 def forward(
     self,
     input_ids: Optional[torch.Tensor] = None,
     attention_mask: Optional[torch.Tensor] = None,
-    past_key_values: Optional[List[Tuple[torch.Tensor,torch.Tensor]]] = None,
+    past_key_values: Optional[List[Tuple[torch.Tensor, torch.Tensor]]] = None,
     use_cache: bool = False,
-    logits_to_keep: Union[int,torch.Tensor] = 0,
-    **args
+    logits_to_keep: Union[int, torch.Tensor] = 0,
+    **args,
 ):
-    h,past_kvs, aux_loss = self.model(
-        input_ids = input_ids,
-        attention_mask = attention_mask,
-        past_key_values = past_key_values,
-        use_cache = use_cache,
-        **args
+    h, past_kvs, aux_loss = self.model(
+        input_ids=input_ids,
+        attention_mask=attention_mask,
+        past_key_values=past_key_values,
+        use_cache=use_cache,
+        **args,
     )
-    slice_indices = slice(-logits_to_keep, None) if isinstance(logits_to_keep, int) else logits_to_keep
-    logits = self.lm_head(h[:slice_indices,:])
-    self.OUT.__setitem__('last_hidden_state',h)
-    self.OUT.__setitem__('logits',logits)
-    self.OUT.__setitem__('aux_loss',aux_loss)
-    self.OUT.__setitem__('past_key_values',past_kvs)
+    slice_indices = (
+        slice(-logits_to_keep, None)
+        if isinstance(logits_to_keep, int)
+        else logits_to_keep
+    )
+    logits = self.lm_head(h[:slice_indices, :])
+    self.OUT.__setitem__("last_hidden_state", h)
+    self.OUT.__setitem__("logits", logits)
+    self.OUT.__setitem__("aux_loss", aux_loss)
+    self.OUT.__setitem__("past_key_values", past_kvs)
     return self.OUT
-
-
